525ba0cdabf0f6a9b3e18c4969561c6438a808ee
GrdnAlex <ag5443@stern.nyu.edu>
2022-07-13 22:35:11 -0400

    Update game2.py

--- game2.py
+++ game2.py
@@ -3,3 +3,29 @@
 print("welcome to rock paper scissors")
 
 
+# User Inputs
+user_choice = input("Please make a selection 'rock', 'paper', 'scissors'): ")
+#You Chose: rock
+print("You chose:", user_choice) 
+
+user_choice = user_choice.lower()
+
+# Validate User Inputs
+
+# Computer Choice
+
+# Determine The Winner
+
+# Display Results
+
+
+# -------------------
+# Welcome 'Player One' to my Rock-Paper-Scissors game...
+# -------------------
+# Please choose either 'rock', 'paper', or 'scissors': rock
+# You chose: 'rock'
+# The computer chose: 'paper'
+# -------------------
+# Oh, the computer won. It's ok.
+# -------------------
+# Thanks for playing. Please play again!
