962d929a6673a028e5932ee15d35f46e8fba830e
GrdnAlex <ag5443@stern.nyu.edu>
2022-07-13 23:00:08 -0400

    Determine the winner

--- game2.py
+++ game2.py
@@ -21,7 +21,23 @@ computer_choice = random.choice(valid_options)
 print("computer_choice:", computer_choice)
 
 # Determine The Winner
-
+if user_choice == computer_choice:
+    print("It's a tie!")
+elif user_choice == "rock":
+    if computer_choice == "scissors":
+        print("Rock crushes scissors. You win!")
+    else:
+        print("Paper covers rock. You lose.")
+elif user_choice == "paper":
+    if computer_choice == "rock":
+        print("Paper covers rock. You win!")
+    else:
+        print("Scissors cuts paper. You lose.")
+elif user_choice == "scissors":
+    if computer_choice == "paper":
+        print("Scissors cuts paper. You win!")
+    else:
+        print("Rock crushes scissors. You lose.")
 # Display Results
 
 
